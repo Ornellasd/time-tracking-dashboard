c53d68cd66934f9fcace177050a7ae11edb257bb
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-09-23 10:06:20 -0700

    add specific activity attributes to each duration button

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ durationButtons.forEach(button => {
   button.addEventListener('click', (e) => {
     e.preventDefault();
 
+    // console.log(button.getAttribute('data-activity'));
+
+    if(button.getAttribute('data-activity') === 'daily') {
+      console.log('DAILY');
+    } else if(button.getAttribute('data-activity') === 'weekly') {
+      console.log('WEEKLY');
+    } else if(button.getAttribute('data-activity') === 'monthly') {
+      console.log('MONTHLY');
+    }
+    // switch(button.getAttribute('data-activity')) {
+    //   case 'daily':
+    //     console.log('DAILY');
+    //   case 'weekly':
+    //     console.log('WEEKLY');
+    //   case 'monthly':
+    //     console.log('MONTHLY');
+    //   default:
+    //     return null;
+    // };
+
+
     durationButtons.forEach(button => {
       button.style.opacity = '0.5';
     });
